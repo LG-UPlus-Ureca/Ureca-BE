12d963ab082920880eab473b0eec09493fbf05c1
JGW-Korea <jgw6372@gmail.com>
2024-09-10 10:16:31 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/com/ureca/lg_uplus_ureca_site_be/LgUplusUrecaSiteBeApplication.java
@@ -0,0 +1,13 @@
+package com.ureca.lg_uplus_ureca_site_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LgUplusUrecaSiteBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LgUplusUrecaSiteBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ureca/lg_uplus_ureca_site_be/LgUplusUrecaSiteBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ureca.lg_uplus_ureca_site_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LgUplusUrecaSiteBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

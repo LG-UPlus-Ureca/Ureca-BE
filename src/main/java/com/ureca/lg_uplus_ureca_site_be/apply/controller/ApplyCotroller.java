2a90d651959d6033fbf325816ca008e49f77f07a
JGW-Korea <jgw6372@gmail.com>
2024-09-11 14:02:28 +0900

    feat: Apply 테이블 DTO 생성

--- src/main/java/com/ureca/lg_uplus_ureca_site_be/apply/controller/ApplyCotroller.java
+++ src/main/java/com/ureca/lg_uplus_ureca_site_be/apply/controller/ApplyCotroller.java
@@ -2,17 +2,15 @@ package com.ureca.lg_uplus_ureca_site_be.apply.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/apply")
 public class ApplyCotroller {
   
-  @GetMapping("/info")
-  public String getMethodName() {
-      return "ㅁㄴㅇㅁㄴㅇㄴㅁㅇ";
+  @PostMapping("/submit")
+  public String postMethodName(@RequestBody String entity) {
+      return entity;
   }
-  
-
 }

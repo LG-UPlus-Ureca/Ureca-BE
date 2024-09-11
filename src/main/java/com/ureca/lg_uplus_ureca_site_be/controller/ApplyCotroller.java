fabb2cbeaea6de747a4ac8fa052daa5ce42209b5
JGW-Korea <jgw6372@gmail.com>
2024-09-11 17:39:34 +0900

    feat: Apply 테이블 삽입 기능 완료

--- src/main/java/com/ureca/lg_uplus_ureca_site_be/controller/ApplyCotroller.java
+++ src/main/java/com/ureca/lg_uplus_ureca_site_be/controller/ApplyCotroller.java
@@ -9,6 +9,7 @@ import com.ureca.lg_uplus_ureca_site_be.service.ApplyService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -25,9 +26,9 @@ public class ApplyCotroller {
 
   // 지원서 제출 할 경우
   @PostMapping("/submit")
-  public Map<String, Object> postMethodName(@RequestBody Apply apply) {
+  public Map<String, Object> postMethodName(@RequestBody Apply apply) throws SQLException {
 
-    Map<String, Object> request = new HashMap<>(); // 결과값 반환
+    Map<String, Object> response = new HashMap<>(); // 결과값 반환
 
     // 지원자 고유한 키 값 생성
     UUID uuid4 = UUID.randomUUID();
@@ -36,8 +37,10 @@ public class ApplyCotroller {
     // 동일한 이름, 전화번호로 지원서를 제출한 경우 -> 중복된 지원 항목 존재 결과 반환
     
     // 위의 조건에 위배되지 않는 경우 -> 지원서 DB에 등록
+    int result = service.registerApplication(apply);
+    System.out.println(result);
 
-      return request;
+    return response;
   }
 
 

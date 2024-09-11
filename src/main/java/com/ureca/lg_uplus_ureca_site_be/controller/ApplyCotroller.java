24208ed606515f6e086f3596944f82b48eda4a86
JGW-Korea <jgw6372@gmail.com>
2024-09-11 16:32:00 +0900

    Apply DAO 기능 구현 완료

--- src/main/java/com/ureca/lg_uplus_ureca_site_be/controller/ApplyCotroller.java
+++ src/main/java/com/ureca/lg_uplus_ureca_site_be/controller/ApplyCotroller.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ureca.lg_uplus_ureca_site_be.dto.Apply;
+import com.ureca.lg_uplus_ureca_site_be.service.ApplyService;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,12 +13,16 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/apply")
 public class ApplyCotroller {
   
+  @Autowired
+  ApplyService service;
+
   // 지원서 제출 할 경우
   @PostMapping("/submit")
   public Map<String, Object> postMethodName(@RequestBody Apply apply) {
--- src/main/java/com/ureca/lg_uplus_ureca_site_be/service/ApplyServiceImpl.java
+++ src/main/java/com/ureca/lg_uplus_ureca_site_be/service/ApplyServiceImpl.java
@@ -2,23 +2,24 @@ package com.ureca.lg_uplus_ureca_site_be.service;
 
 import java.sql.SQLException;
 
-// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ureca.lg_uplus_ureca_site_be.dao.ApplyDAO;
 import com.ureca.lg_uplus_ureca_site_be.dto.Apply;
 
 @Service
 public class ApplyServiceImpl implements ApplyService {
 
-  // @Autowired
-  // ApplyDAO dao
+  @Autowired
+  ApplyDAO dao;
 
-  @Override
+  @Override // 중복된 지원서 항목 존재 여부 반환
   public Apply checkOverlapApply(String name, String phoneNumber) throws SQLException {
     return dao.checkOverlap(name, phoneNumber);
   }
 
-  @Override
+  @Override // 새로운 지원서 항목 생성 여부 반환
   public int registerApplication(Apply newList) throws SQLException {
     return dao.registerApply(newList);
   }
